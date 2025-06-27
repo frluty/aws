1779debb21ee10b874ae5cd9a78d18c154747b73
Luty FRANCOIS <francoisluty@gmail.com>
2025-06-27 19:11:53 +0000

    int

---
+++ Terraform/variable.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type = string
+}
+
+variable "profile" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
